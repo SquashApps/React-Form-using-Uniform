29311ce27b167531ba8cd4a11d4f9982eab9b3c7
venkatesh <venkatesh@squashapps.com>
2018-11-28 12:06:54 +0530

    Worked on handling the crash error on the selection of second date

--- imports/components/TimeRange.js
+++ imports/components/TimeRange.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import connectField from 'uniforms/connectField';
 import { TimePicker } from 'antd';
+import moment from 'moment';
 
 const startAndEndTime = (time, value, onChange) => {
     value['start'] = new Date(time);
@@ -11,11 +12,15 @@ const checkStartAndEndTime = (time, value, onChange) => {
     onChange(value);
 }
 const disableHours = (value) => {
+    if(value['start']){
     let timeLimit = value['start'].getHours();
     return Array.apply(null, { length: timeLimit }).map(Number.call, Number)
+    }
+    
 }
 
 const TimeRange = ({ onChange, value }) => {
+    console.log('inside time range component',value['start'])
     return (<section className="time_range">
         <label>Time</label>
         <TimePicker onChange={start => startAndEndTime(start, value, onChange)} />
